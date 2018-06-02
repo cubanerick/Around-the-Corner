2c75479a26f7f67742f7d3dadf86144137c8c7f3
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-06-02 11:56:11 -0700

    eventbrite ajax setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+$(document).on("click","#searchBtn", function() {
+    event.preventDefault();
+
+    var searchInput = $("#searchInput").val().trim();
+    var location = $("#location").val().trim();
+    var radius = $("#radius").val().trim();
+
+    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=NFYIPZGRL3ENLJ7TMLZJ&q=" + searchInput + "&location.address=" + location + "&location.within=" + radius;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        
+        
+        var results = response.events;
+        
+        for (var i = 0; i < results.length; i++) {
+
+            $("#object").append(response.events[i].venue_id);
+            $("#object").append(response.events[i].description.text);
+            $("#object").append(response.events[i].url);
+
+            var venueID = response.events[i].venue_id;
+            var venuesQueryURL = 'https://www.eventbriteapi.com/v3/venues/' + venueID + '/?token=NFYIPZGRL3ENLJ7TMLZJ';
+            
+            $.ajax({
+                url: venuesQueryURL,
+                method : 'GET'
+            }).then(function(response) {
+                
+                var venueAddress = response.address.localized_address_display;
+                var latitude = response.address.latitude;
+                var longitude = response.address.longitude;
+
+                $("#addresses").append(venueAddress);
+
+                console.log(venueAddress);
+                
+            })
+
+        }
+
+    });
+});
