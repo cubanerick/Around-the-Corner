f902509dc9ca004d8d1dd2e867eb145ca8150d29
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-04 20:24:58 -0700

    added sidebar to maps.html

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -47,3 +47,10 @@ function initMap() {
 $(document).on('click','#searchPageButton', function() {
     window.location.href = 'index.html';
 })
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
